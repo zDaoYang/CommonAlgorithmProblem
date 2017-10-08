efd5ca4cb76e1a91641f4271dcd74fae0d7d732e
zDaoYang <zDaoYang@users.noreply.github.com>
2017-10-09 02:47:20 +0800

    undate 字符串

--- 字符串/互为变形词.js
+++ 字符串/互为变形词.js
@@ -1,15 +1,24 @@
-//　两个字符串中的字符种类一样，并且每种字符出现的次数一样，那么就互为变形词
-// 解法，利用hash表，键为字符种类，值为特定字符种类出现的次数
-function chkTransform(str1, str2) {
-  if(str1.length !== str2.length) {
+/*
+给定两个字符串，如果它们的字符种类一样，并且每种字符出现的次数一样，那么就互为变形词
+例如：'ABDD' 和 'DDBA' 互为变形词，而'ABDD' 和 'ABDE' 不互为变形词
+
+解法:
+
+1、如果传入参数个数不为2或者两个字符串长度不相等, 很显然，直接返回false
+2、记录两个字符中字符的种类和对应出现的次数(一种键值对的形式，我们自然想到用对象来实现)
+3、如果这两个对象完全相同，返回true，否则，返回false，问题又来了，JS里的对象是引用类型，不管两个对象内容是否一致，它们是始终不相等的
+4、调用JSON.stringify()将对象序列化可以解决3中的问题
+*/
+function isTranform(str1, str2) {
+  if(arguments.length !== 2 || str1.length !== str2.length) {
     return false;
   }
-  var hash1 = {}, hash2 = {}, len = str1.length;
+  var hash1 = [], hash2 = [], len = str1.length;
   for(var i = 0; i < len; i++) {
-    if(hash1[str1[i]]) {
-      hash1[str1[i]]++;
-    } else {
-      hash1[str1[i]] = 1;
+    if(hash1[str1[i]]) { 
+      hash1[str1[i]]++; // 如果已经存在这个字符，则对应的数量加1
+    } else { 
+      hash1[str1[i]] = 1; // 如果之前没有这个字符，那么就初始化为1
     }
     if(hash2[str2[i]]) {
       hash2[str2[i]]++;
@@ -21,6 +30,17 @@ function chkTransform(str1, str2) {
 }
 
 var str1 = '1235544';
-var str2 = '3215544';
+var str2 = '3214455';
+var str3 = '1235554';
+console.log(isTranform(str1, str2));  // true
+console.log(isTranform(str1, str3));  // false
 
-console.log(chkTransform(str1, str2));
+/*
+
+多说一句，第16行的var hash1 = {}, hash2 = {} 其实也可以换成
+var hash1 = [], hash2 = []，因为js里数组的下标也可以是字符串
+例如var arr = []; arr['a'] = 1 是合法的
+但是我们要避免这种做法，由于某种特殊原因，比如arr['3'] = 1的时候，
+arr为[ undefined, undefined, undefined, 1 ]，会浪费额外的内存空间
+
+*/
