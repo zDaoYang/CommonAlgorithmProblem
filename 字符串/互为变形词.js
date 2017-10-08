8ad24313599aa51d678ec31fd27eb5414aa21097
zDaoYang <zDaoYang@users.noreply.github.com>
2017-10-09 02:50:31 +0800

    update 字符串

--- 字符串/互为变形词.js
+++ 字符串/互为变形词.js
@@ -6,7 +6,7 @@
 
 1、如果传入参数个数不为2或者两个字符串长度不相等, 很显然，直接返回false
 2、记录两个字符中字符的种类和对应出现的次数(一种键值对的形式，我们自然想到用对象来实现)
-3、如果这两个对象完全相同，返回true，否则，返回false，问题又来了，JS里的对象是引用类型，不管两个对象内容是否一致，它们是始终不相等的
+3、如果这两个对象完全相同，返回true，否则，返回false，问题又来了，JS里的对象是引用类型，不管两个对象的内容是否一致，它们是始终不相等的
 4、调用JSON.stringify()将对象序列化可以解决3中的问题
 */
 function isTranform(str1, str2) {
@@ -37,10 +37,9 @@ console.log(isTranform(str1, str3));  // false
 
 /*
 
-多说一句，第16行的var hash1 = {}, hash2 = {} 其实也可以换成
-var hash1 = [], hash2 = []，因为js里数组的下标也可以是字符串
-例如var arr = []; arr['a'] = 1 是合法的
-但是我们要避免这种做法，由于某种特殊原因，比如arr['3'] = 1的时候，
-arr为[ undefined, undefined, undefined, 1 ]，会浪费额外的内存空间
+多说一句，第16行的var hash1 = {}, hash2 = {} 其实也可以换成var hash1 = [], hash2 = []，
+因为js里数组的下标也可以是字符串，例如var arr = []; arr['a'] = 1 是合法的。
+但是我们要避免这种做法，由于某种特殊原因，比如arr['3'] = 1的时候，arr为[ undefined, undefined, undefined, 1 ]，
+如果这些undefined在后面得不到填补的话，会浪费额外的内存空间
 
 */
